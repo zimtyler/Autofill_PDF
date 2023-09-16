834571cfbe384775cc2a4471cf68b6448ee9ee9c
zimtyler <101671431+zimtyler@users.noreply.github.com>
2023-09-16 10:45:44 -0400

    Update Autofill_PDF_Python.py

--- Autofill_PDF_Python.py
+++ Autofill_PDF_Python.py
@@ -1,6 +1,9 @@
 import pdfrw
 import pandas as pd
-
+import os
+import glob
+from google.cloud import bigquery
+from google.cloud.bigquery import magics
 
 ANNOT_KEY = '/Annots'
 ANNOT_NAME_KEY = '/Name'
@@ -10,96 +13,78 @@ ANNOT_RECT_KEY = 'Rect'
 SUBTYPE_KEY = '/Subtype'
 WIDGET_SUBTYPE_KEY = '/Widget'
 
-
-def autofill_bulk_pdf(input_pdf_path, client_csv):
-    client_df = pd.read_csv(client_csv)
-    client_dict = client_df.to_dict('index')
-    template_pdf = pdfrw.PdfReader(input_pdf_path)
-    for x, y in client_dict.items():
-        individual_dict = y
-        for page in template_pdf.pages:
-            annotations = page[ANNOT_KEY]
-            for annotation in annotations:
-                if annotation[SUBTYPE_KEY] == WIDGET_SUBTYPE_KEY:
-                    if annotation[ANNOT_FIELD_KEY]:
-                        key = annotation[ANNOT_FIELD_KEY][1:-1]
-                        if key in individual_dict.keys():
-                            annotation.update(
-                                pdfrw.PdfDict(V='{}'.format(individual_dict[key])))
-                                
-                            annotation.update(pdfrw.PdfDict(AP=''))
-                            
-                        '''For some reason, the PdfReader will not pull up an Annot if it is
-                        listed more than once in the PDF. This also applies to 'exclusive or' 
-                        check boxes. This is why I had to apply name to a different key ('Owner Name')'''
-                       
-                        if key == 'Owner Name':
-                            annotation.update(
-                                pdfrw.PdfDict(V='{}'.format(individual_dict['Name'])))
-                                
-                            annotation.update(pdfrw.PdfDict(AP=''))
-        template_pdf.Root.AcroForm.update(pdfrw.PdfDict(NeedAppearances=pdfrw.PdfObject('true')))
-        pdfrw.PdfWriter().write('{} Review.pdf'.format(individual_dict['Name']), template_pdf)
-
-autofill_bulk_pdf("C:Automation_Projects\Review_Form.pdf", "C:Automation_Projects\Client_Review_List_FINAL.csv")
-
-
-
-def autofill_individual_pdf(input_pdf_path, client_csv, name):
-    client_df = pd.read_csv(client_csv)
-    client_dict = client_df.to_dict('index')
-    template_pdf = pdfrw.PdfReader(input_pdf_path)
-    client_df = pd.read_csv(client_csv)
-    name_list = client_df['Name'].values.tolist()
-    i = name_list.index(name)
-    individual_dict = client_dict[i]
-    for page in template_pdf.pages:
-        annotations = page[ANNOT_KEY]
-        for annotation in annotations:
-            if annotation[SUBTYPE_KEY] == WIDGET_SUBTYPE_KEY:
-                if annotation[ANNOT_FIELD_KEY]:
-                    key = annotation[ANNOT_FIELD_KEY][1:-1]
-                    if key in individual_dict.keys():
-                        annotation.update(
-                            pdfrw.PdfDict(V='{}'.format(individual_dict[key])))
-                                
-                        annotation.update(pdfrw.PdfDict(AP=''))
-                    if key == 'Owner Name':
-                        annotation.update(
-                            pdfrw.PdfDict(V='{}'.format(individual_dict['Name'])))
-                                
-                        annotation.update(pdfrw.PdfDict(AP=''))
-    template_pdf.Root.AcroForm.update(pdfrw.PdfDict(NeedAppearances=pdfrw.PdfObject('true')))
-    pdfrw.PdfWriter().write('{} Review.pdf'.format(individual_dict['Name']), template_pdf)
-
-autofill_individual_pdf("C:Automation_Projects\Review_Form.pdf", "C:Automation_Projects\Client_Review_List_FINAL.csv", "John Doe")
-
-def update_multiple_pdf(input_pdf_path, client_csv, *args):
-    client_df = pd.read_csv(client_csv)
-    client_dict = client_df.to_dict('index')
-    template_pdf = pdfrw.PdfReader(input_pdf_path)
-    client_df = pd.read_csv(client_csv)
-    name_list = client_df['Name'].values.tolist()
-    for name in args:
-        i = name_list.index(name)
-        individual_dict = client_dict[i]
-        for page in template_pdf.pages:
-            annotations = page[ANNOT_KEY]
-            for annotation in annotations:
-                if annotation[SUBTYPE_KEY] == WIDGET_SUBTYPE_KEY:
-                    if annotation[ANNOT_FIELD_KEY]:
-                        key = annotation[ANNOT_FIELD_KEY][1:-1]
-                        if key in individual_dict.keys():
-                            annotation.update(
-                                pdfrw.PdfDict(V='{}'.format(individual_dict[key])))
-                                    
-                            annotation.update(pdfrw.PdfDict(AP=''))
-                        if key == 'Owner Name':
-                            annotation.update(
-                                pdfrw.PdfDict(V='{}'.format(individual_dict['Name'])))
-                                    
-                            annotation.update(pdfrw.PdfDict(AP=''))
-        template_pdf.Root.AcroForm.update(pdfrw.PdfDict(NeedAppearances=pdfrw.PdfObject('true')))
-        pdfrw.PdfWriter().write('{} Review.pdf'.format(individual_dict['Name']), template_pdf)
-update_multiple_pdf("C:Automation_Projects\Review_Form.pdf", "C:Automation_Projects\Client_Review_List_FINAL.csv", "John Doe")
+creds = getCredentials(name, json_path)
+user = bigquery.Client.from_service_account_json(creds)
+qStr = """
+    select
+        name, dob, phoneNum, address,
+        max(case when rn = 1 then acctNum end) as Account_1,
+        max(case when rn = 2 then acctNum end) as Account_2,
+        max(case when rn = 3 then acctNum end) as Account_3,
+        max(case when rn = 4 then acctNum end) as Account_4)
+        from (
+            select
+            name,
+            dob,
+            phoneNum
+            acctNum,
+            row_number() over (partition by name order by acctNum) as rn
+            from client_info) ci
+    group by name
+    order By name;
+"""
+def getClientDF(query_string, user):
+    queryData = user.query(query_string)
+    df = queryData.to_dataframe()
+    return df
     
+def updateClientPDF(inCloud=True, input_path, output_path):
+    template_pdf = pdfrw.PdfReader(input_pdf_path).Root.AcroForm.update(
+            pdfrw.PdfDict(NeedAppearances=pdfrw.PdfObject('true'))
+        )
+    if inCloud:
+        client_df = getClientDF()
+    else
+        client_csv = glob.glob(os.path.join(input_path, "client_csv.csv"))
+        client_df = pd.read_csv(client_csv)
+        # client_dict = client_df.to_dict("index")
+        template_pdf = pdfrw.PdfReader(input_pdf_path).Root.AcroForm.update(
+            pdfrw.PdfDict(NeedAppearances=pdfrw.PdfObject('true'))
+        )
+        name_list = client_df["name"].values.tolist()
+        for name in args:
+            i = name_list.index(name)
+            individual_dict = client_dict[i]
+            for page in template_pdf.pages:
+                annotations = page[ANNOT_KEY]
+                for annotation in annotations:
+                    if annotation[SUBTYPE_KEY] == WIDGET_SUBTYPE_KEY:
+                        if annotation[ANNOT_FIELD_KEY]:
+                            key = annotation[ANNOT_FIELD_KEY][1:-1]
+                            if key in individual_dict.keys():
+                                annotation.update(
+                                    pdfrw.PdfDict(V="{}".format(individual_dict[key]))
+                                )       
+                                annotation.update(pdfrw.PdfDict(AP=""))
+                            # PDF Template uses two separate name fields.
+                            # FINRA Regs == Wary of updating template.
+                            if key == "Owner Name":
+                                annotation.update(
+                                    pdfrw.PdfDict(V='{}'.format(individual_dict["Name"]))
+                                )
+                                annotation.update(pdfrw.PdfDict(AP=""))
+            # template_pdf.Root.AcroForm.update(pdfrw.PdfDict(NeedAppearances=pdfrw.PdfObject('true')))
+            pdfrw.PdfWriter().write("{} Review.pdf".format(individual_dict["Name"]), template_pdf)
+    updateClientPDF("C:Automation_Projects")
+
+def main():
+    inCloud = input("Query? yes or no: ")
+    inCloudBool = inClound.lower()[0] == "y"
+    
+    input_path = input("Provide path to directory containing template and/or csvs: ")
+    output_path = input{"Provide desired output directory: ")
+    updateClientPDF(inCloud=inCloudBool, input_path, output_path)
+    
+if __name__ == '__main__':
+    main()
+        
